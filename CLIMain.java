4773f82f019156b1132082c11cdbdadc1da353f8
Jakob Dingeldey <Jako.Dinge@bso.int>
2023-07-17 08:07:25 +0200

    CLIMain ueberarbeitet

--- CLIMain.java
+++ CLIMain.java
@@ -7,11 +7,11 @@ public class CLIMain {
   public static void main(String[] args) {    
     Scanner scanner = new Scanner(System.in);
     boolean weiter = true;
+    System.out.println("Erlaubte Aktionen:");
+    System.out.println(" - Figur setzen: \"set (x) (y) ('x'/'y')\"; e.g.: \"set 1 1 x\"");  
+    System.out.println(" - Figur erhalten: \"get (x) (y) ('x'/'y')\"; e.g.: \"get 1 1\""); 
+    System.out.println(" - Abbrechen: \"stop\""); 
     while (weiter) {
-      System.out.println("Erlaubte Aktionen:");
-      System.out.println(" - Figur setzen: \"set (x) (y) ('x'/'y')\"; e.g.: \"set 1 1 x\"");  
-      System.out.println(" - Figur erhalten: \"get (x) (y) ('x'/'y')\"; e.g.: \"get 1 1\""); 
-      System.out.println(" - Abbrechen: \"stop\""); 
       System.out.println("  Welche Aktion soll ausgeführt werden?");
       String line = scanner.nextLine();
       if (line.startsWith("set ")) {
