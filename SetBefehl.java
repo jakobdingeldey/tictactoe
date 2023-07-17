9fd0b3b1d6cf2bed0a2ea4e1e37b66b012991237
Jakob Dingeldey <Jako.Dinge@bso.int>
2023-07-17 08:58:45 +0200

    SetBefehl ueberarbeitet

--- SetBefehl.java
+++ SetBefehl.java
@@ -1,6 +1,6 @@
 public class SetBefehl {
   public static void ausfuehren(String[] args) {
-    if (args.length != 4 && (!args[3].equals("o") || !args[3].equals("x"))) {
+    if (args.length != 4 || (!args[3].equals("o") || !args[3].equals("x"))) {
       System.err.println("Es werden drei Argumente erwartet: (x) (y) ('x'/'o'); Beispiel: 'set 1 2 o'");
       return;
     }
